18ef468ced1ac7169088e6a9f550f491e2ae97c9
André Luís Menegatti <andre.menegatti@hotmail.com>
2020-06-07 20:22:16 -0300

    Correlation plots

--- corr-plot-agua-esgoto.R
+++ corr-plot-agua-esgoto.R
@@ -1,52 +1,39 @@
 library(tidyverse)
 library(reshape2)
-library(cagedExplorer)
-
-# Loading and filtering dataset -----------------------------------------------
-snis <- readRDS('snis-2018-clean.rds')
-
-snis_agua <- snis %>% 
-  filter(!(municipio_clean %in% 
-             c('MAUA', 'SALTO', 'SANTA MARIA DA SERRA') &
-             tipo_servico == 'Esgotos'))
-
-snis_esgoto <- snis %>% 
-  filter(!(municipio_clean %in% 
-             c('MAUA', 'SALTO', 'SANTA MARIA DA SERRA') &
-             tipo_servico == 'Água'))
-
-# Custom map theme and settings -----------------------------------------------
+source('helpers.R')
 theme_set(custom_theme())
 
+# Loading and filtering dataset -----------------------------------------------
+snis <- readRDS('data/snis-bahia.rds')
+
+snis_esgoto <- snis %>% filter(tipo_servico != 'Água') %>% 
+  filter(!(municipio == 'Araci' & sigla_prestador == 'PMA'))
+
 # Subsetting: only relevant columns, with shorter names -----------------------
-# Water and 'overall' features
-snis_subset_agua <- snis_agua %>% 
-  select(municipio_clean,
-         tarifa_media = in004_tarifa_media_praticada,
+snis_subset <- snis_esgoto %>% 
+  select(municipio,
+         pib_per_capita = pib_per_capita2017,
          tarifa_media_agua = in005_tarifa_media_de_agua,
+         tarifa_media_esgoto = in006_tarifa_media_de_esgoto,
          perdas_agua = in013_indice_de_perdas_faturamento,
          atendimento_agua = in055_indice_de_atendimento_total_de_agua,
+         coleta_esgoto = in015_indice_de_coleta_de_esgoto,
+         tratamento_esgoto = in046_indice_de_esgoto_tratado_referido_a_agua_consumida,
          consumo_medio_per_capita_agua = in022_consumo_medio_percapita_de_agua,
          investimento_per_capita = inv_per_capita,
          desempenho_financeiro = in012_indicador_de_desempenho_financeiro
   )
 
-# Sewage-related features
-snis_subset_esgoto <- snis_esgoto %>% 
-  select(municipio_clean,
-         tarifa_media_esgoto = in006_tarifa_media_de_esgoto,
-         coleta_esgoto = in015_indice_de_coleta_de_esgoto,
-         tratamento_esgoto = in016_indice_de_tratamento_de_esgoto,
-  )
-
-# Concatenating partial datasets
-snis_subset <- snis_subset_agua %>% 
-  left_join(snis_subset_esgoto, by = 'municipio_clean')
+dim(snis_subset)
 
 # Computing the correlation matrix --------------------------------------------
-corr_matrix <- snis_subset %>% 
-  select(-municipio_clean) %>% 
-  drop_na() %>% 
+snis_complete_variables <- snis_subset %>% 
+  select(-municipio) %>% 
+  drop_na()
+
+dim(snis_complete_variables)
+
+corr_matrix <- snis_complete_variables %>% 
   cor() %>% 
   round(2)
 
@@ -70,12 +57,13 @@ pretty_labels <- function(x) {
     str_replace('tarifa_media_esgoto', 'Tarifa média (esgoto)') %>% 
     str_replace('tarifa_media$', 'Tarifa média (total)') %>% 
     str_replace('tarifa_media_agua', 'Tarifa média (água)') %>% 
-    str_replace('coleta_esgoto', 'Coleta de esgoto') %>% 
-    str_replace('tratamento_esgoto', 'Trat. de esgoto') %>% 
-    str_replace('consumo_medio_per_capita_agua', 'Cons. de água per capita') %>% 
-    str_replace('atendimento_agua' ,'Atend. de água') %>% 
+    str_replace('coleta_esgoto', 'Coleta (esgoto)') %>% 
+    str_replace('tratamento_esgoto', 'Tratamento (esgoto)') %>% 
+    str_replace('consumo_medio_per_capita_agua', 'Cons. per capita (água)') %>% 
+    str_replace('atendimento_agua' ,'Atendimento (água)') %>% 
     str_replace('desempenho_financeiro', 'Desemp. financeiro') %>% 
-    str_replace('perdas_agua', 'Perdas (faturamento)')
+    str_replace('perdas_agua', 'Perdas de faturamento') %>% 
+    str_replace('pib_per_capita', 'PIB per capita')
 }
 
 # Redefining factor labels for pretty names
@@ -86,19 +74,26 @@ melted_cormat_pretty <- melted_cormat %>%
 # Plotting
 corr_plot <- 
   ggplot(data = melted_cormat_pretty,
-       aes(x = Var2, y = Var1, fill = value)) +
+         aes(x = Var2, y = Var1, fill = value)) +
   geom_tile(color = 'white') +
   geom_text(
     aes(x = Var2, y = Var1,
         label = formatC(value, big.mark = '.', decimal.mark = ',')),
     color = "gray10", size = 3.25, family = 'serif'
-    ) +
+  ) +
+  geom_text(
+    data = melted_cormat_pretty %>% filter(value >= .9),
+    aes(x = Var2, y = Var1,
+        label = formatC(value, big.mark = '.', decimal.mark = ',')),
+    color = "gray90", size = 3.25, family = 'serif'
+  ) +
   scale_fill_gradient2(
-    low = '#053061', high  = '#b2182b', mid = 'white',
+    high = '#053061', low  = '#67000d', #'#b2182b',
+    mid = 'white',
     midpoint = 0, limit = c(-1, 1), space = 'Lab',
     name = "Coeficiente\nde correlação",
     label = function(x) formatC(x, big.mark = '.', decimal.mark = ',')
-    ) +
+  ) +
   coord_fixed() +
   theme_minimal() +
   theme(
@@ -113,11 +108,12 @@ corr_plot <-
     panel.grid = element_blank(),
     legend.position = 'top',
     legend.direction = 'horizontal'
-    ) +
+  ) +
   guides(fill = guide_colorbar(
     barwidth = 7, barheight = 1,
     title.position = 'top', title.hjust = 0.5
-    )) ; corr_plot
+  )) ; corr_plot
 
 # Saving
-ggsave('plots/corr-plot.png', corr_plot, width = 6, height = 6)
+ggsave('plots/esgoto/corr-plot-esgoto.png', corr_plot,
+       width = 6, height = 6)
