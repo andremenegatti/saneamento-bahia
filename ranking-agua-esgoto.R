6eac3a532691e62014e9aac5fcea89c84827189b
André Luís Menegatti <andre.menegatti@hotmail.com>
2020-06-26 17:41:02 -0300

    Correcting normalization (rankings)

--- ranking-agua-esgoto.R
+++ ranking-agua-esgoto.R
@@ -22,25 +22,43 @@ snis_ranking <- snis_esgoto %>%
 
 snis_ranking <- snis_ranking %>% 
   drop_na() %>% 
-  mutate(perdas = ifelse(perdas > 0, perdas, 0)) %>% # <----
   mutate(
+  perdas_trunc = ifelse(perdas > 0, perdas, 0),
+  perdas_trunc_norm = (max(perdas_trunc) - perdas_trunc) /
+    (max(perdas_trunc) - min(perdas_trunc)) * 100,
+  perdas_norm = (max(perdas) - perdas) /
+    (max(perdas) - min(perdas)) * 100,
   tarifa_agua_norm = (max(tarifa_agua) - tarifa_agua) / 
     (max(tarifa_agua) - min(tarifa_agua)) * 100,
   tarifa_esgoto_norm = (max(tarifa_esgoto) - tarifa_esgoto) /
-    (max(tarifa_esgoto) - min(tarifa_esgoto) * 100),
+    (max(tarifa_esgoto) - min(tarifa_esgoto)) * 100,
   inv_per_capita_norm = (inv_per_capita - min(inv_per_capita)) /
     (max(inv_per_capita) - min(inv_per_capita)) * 100,
-  perdas_norm = (perdas - min(perdas)) /
-    (max(perdas) - min(perdas)) * 100,
   desempenho_norm = (desempenho - min(desempenho)) / 
     (max(desempenho) - min(desempenho)) * 100,
   # Score as average of normalized indicators
   score = (atendimento + coleta + tratamento +
              tarifa_agua_norm + tarifa_esgoto_norm +
-             inv_per_capita_norm + perdas_norm + desempenho_norm) / 8
-) %>% 
-  mutate(ranking = dense_rank(desc(score))) %>% 
+             inv_per_capita_norm + perdas_norm + desempenho_norm) / 8,
+  score_perdas_trunc = (atendimento + coleta + tratamento +
+                          tarifa_agua_norm + tarifa_esgoto_norm +
+                          inv_per_capita_norm + perdas_trunc_norm + 
+                          desempenho_norm) / 8
+  ) %>% 
+  mutate(ranking = dense_rank(desc(score)),
+         ranking_perdas_trunc = dense_rank(desc(score_perdas_trunc))) %>% 
   arrange(ranking)
+
+# snis_ranking %>% select(contains('norm'), coleta, tratamento, atendimento) %>% summary()
+
+# Saving results
+snis_ranking %>% 
+  select(municipio, ranking, ranking_perdas_trunc,
+         score, score_perdas_trunc, tipo_prestador, populacao,
+         desempenho, desempenho_norm, inv_per_capita, inv_per_capita_norm,
+         tarifa_agua, tarifa_agua_norm, tarifa_esgoto, tarifa_esgoto_norm,
+         coleta, tratamento, perdas, perdas_norm, perdas_trunc_norm) %>% 
+  write_excel_csv2('data/ranking/ranking-agua-esgoto.csv')
 
 # Table with top 10 and worst 10 cities ---------------------------------------
 highlights_agua_esgoto <- snis_ranking %>% 
@@ -53,21 +71,20 @@ highlights_agua_esgoto <- snis_ranking %>%
 
 # Persisting highlights (csv and rds)
 highlights_agua_esgoto %>% 
-  write.csv(file = 'data/highlights_agua_esgoto.csv', row.names = FALSE)
-write_rds(highlights_agua_esgoto, 'data/highlights_agua_esgoto.rds')
+  write_excel_csv2('data/ranking/highlights-agua-esgoto.csv')
+write_rds(highlights_agua_esgoto, 'data/ranking/highlights-agua-esgoto.rds')
 
 # Plotting score distribution -------------------------------------------------
 salvador <- snis_ranking %>% filter(municipio == 'Salvador') %>% pull(score)
-salvador_height <- 10
+salvador_height <- 5
 mediana <- snis_ranking$score %>% median()
-mediana_height <- 15
-# juazeiro <- snis_ranking %>% filter(municipio == 'Juazeiro') %>% pull(score)
+mediana_height <- 5
 
 hist_ranking_agua_esgoto <- snis_ranking %>% 
   ggplot() +
   geom_histogram(
     aes(x = score, fill = tipo_prestador),
-    bins = 15
+    bins = 12
     ) +
   geom_path(
     data = tibble(x = rep(salvador, 2),y = c(0, salvador_height)),
@@ -77,7 +94,7 @@ hist_ranking_agua_esgoto <- snis_ranking %>%
     x = salvador, y = salvador_height, color = 'gray15', fill = 'gray97',
     label = str_c('Salvador:\n', round(salvador, 1)) %>% 
       str_replace('\\.', ','),
-    family = 'serif', size = 3.5
+    family = 'serif', size = 3
   ) +
   geom_path(
     data = tibble(x = rep(mediana, 2),y = c(0, mediana_height)),
@@ -87,31 +104,38 @@ hist_ranking_agua_esgoto <- snis_ranking %>%
     x = mediana, y = mediana_height, color = 'gray15', fill = 'gray97',
     label = str_c('Mediana:\n', round(mediana, 1)) %>% 
       str_replace('\\.', ','),
-    family = 'serif', size = 3.5
+    family = 'serif', size = 3
   ) +
   theme(panel.grid = element_blank(),
         legend.title = element_blank(),
-        legend.position = c(.83, .90)) +
+        legend.position = c(.175, .90)) +
   scale_fill_manual(values = c('#fed976', '#ef3b2c')) +
   scale_x_continuous(breaks = seq(20, 70, by = 10)) +
-  coord_cartesian(xlim = c(20, 70)) +
   labs(
     x = 'Média dos indicadores normalizados (0 a 100)',
     y = 'Número de municípios',
     title = 'Qualidade dos serviços de saneamento básico (água e esgoto)',
     subtitle = 'Distribuição das notas dos municípios baianos'
-  ) ; hist_ranking_agua_esgoto
+  )
+
+hist_ranking_agua_esgoto
 
 # Saving
 ggsave(plot = hist_ranking_agua_esgoto, width = 6, height = 5,
-       filename = 'plots/histogram-ranking-agua-esgoto.png')
+       filename = 'plots/ranking/histogram-ranking-agua-esgoto.png')
 
-# Average by provider type
-snis_ranking %>% 
+
+# Average by provider type ----------------------------------------------------
+avg_by_provider_type <- snis_ranking %>% 
   group_by(tipo_prestador) %>% 
   summarise(n = n(),
             avg_pop = mean(populacao),
             median_pop = median(populacao),
+            media_simples = mean(score),
             media_ponderada = weighted.mean(score, w = populacao),
-            media_simples = mean(score)) %>% 
-  ungroup()
+            mediana = median(score)) %>% 
+  ungroup() ; avg_by_provider_type
+
+# Saving
+write_excel_csv2(avg_by_provider_type,
+                 'data/ranking/score-agua-esgoto-avg-provider-type.csv')
--- ranking-agua.R
+++ ranking-agua.R
@@ -17,22 +17,38 @@ snis_ranking <- snis_agua %>%
          desempenho = in012_indicador_de_desempenho_financeiro
          ) %>% 
   drop_na() %>% 
-  mutate(perdas = ifelse(perdas > 0, perdas, 0)) %>% # <----
   mutate(
+    perdas_trunc = ifelse(perdas > 0, perdas, 0),
+    perdas_trunc_norm = (max(perdas_trunc) - perdas_trunc) /
+      (max(perdas_trunc) - min(perdas_trunc)) * 100,
+    perdas_norm = (max(perdas) - perdas) /
+      (max(perdas) - min(perdas)) * 100,
     tarifa_agua_norm = (max(tarifa_agua) - tarifa_agua) / 
       (max(tarifa_agua) - min(tarifa_agua)) * 100,
     inv_per_capita_norm = (inv_per_capita - min(inv_per_capita)) /
       (max(inv_per_capita) - min(inv_per_capita)) * 100,
-    perdas_norm = (perdas - min(perdas)) /
-      (max(perdas) - min(perdas)) * 100,
     desempenho_norm = (desempenho - min(desempenho)) / 
       (max(desempenho) - min(desempenho)) * 100,
     # Score as average of normalized indicators
     score = (atendimento + tarifa_agua_norm + inv_per_capita_norm +
-      perdas_norm + desempenho_norm) / 5
+               perdas_norm + desempenho_norm) / 5,
+    score_perdas_trunc = (atendimento + tarifa_agua_norm + inv_per_capita_norm +
+                perdas_trunc_norm + desempenho_norm) / 5
   ) %>% 
-  mutate(ranking = dense_rank(desc(score))) %>% 
+  mutate(
+    ranking = dense_rank(desc(score)),
+    ranking_perdas_trunc = dense_rank(desc(score_perdas_trunc))
+    ) %>% 
   arrange(ranking)
+
+# Saving results
+snis_ranking %>% 
+  select(municipio, ranking, #ranking_perdas_trunc,
+         score, #score_perdas_trunc,
+         tipo_prestador, populacao,
+         desempenho, desempenho_norm, inv_per_capita, inv_per_capita_norm,
+         tarifa_agua, tarifa_agua_norm, perdas, perdas_norm, perdas_trunc_norm) %>% 
+  write_excel_csv2('data/ranking/ranking-agua-383municipios.csv')
 
 # Table with top 10 and worst 10 cities ---------------------------------------
 highlights_agua <- snis_ranking %>% 
@@ -44,29 +60,18 @@ highlights_agua <- snis_ranking %>%
             .funs = partial(round, digits = 2)) ; highlights_agua
 
 # Persisting highlights (csv and rds files)
-write.csv(highlights_agua, file = 'data/highlights_agua.csv', row.names = FALSE)
-saveRDS(highlights_agua, 'data/highlights_agua.rds')
+write_excel_csv2(highlights_agua, 'data/ranking/highlights-agua.csv')
+saveRDS(highlights_agua, 'data/ranking/highlights-agua.rds')
 
 # Plotting score distribution -------------------------------------------------
 salvador <- snis_ranking %>% filter(municipio == 'Salvador') %>% pull(score)
-height_salvador <- 25
+height_salvador <- 10
 mediana <- snis_ranking$score %>% median()
-height_mediana <- 10
-# juazeiro <- snis_ranking %>% filter(municipio == 'Juazeiro') %>% pull(score)
+height_mediana <- 20
 
 hist_ranking_agua <- snis_ranking %>% 
   ggplot() +
   geom_histogram(aes(x = score, fill = tipo_prestador), bins = 30) +
-  geom_path(
-    data = tibble(x = rep(salvador, 2),y = c(0, height_salvador)),
-    mapping = aes(x = x, y = y), linetype = 'dotted', alpha = .8,
-  ) +
-  geom_label(
-    x = salvador, y = height_salvador, color = 'gray15', fill = 'gray97',
-    label = str_c('Salvador:\n', round(salvador, 1)) %>% 
-      str_replace('\\.', ','),
-    family = 'serif', size = 3.5
-  ) +
   geom_path(
     data = tibble(x = rep(mediana, 2),y = c(0, height_mediana)),
     mapping = aes(x = x, y = y), linetype = 'dotted', alpha = .8,
@@ -75,28 +80,48 @@ hist_ranking_agua <- snis_ranking %>%
     x = mediana, y = height_mediana, color = 'gray15', fill = 'gray97',
     label = str_c('Mediana:\n', round(mediana, 1)) %>% 
       str_replace('\\.', ','),
-    family = 'serif', size = 3.5
+    family = 'serif', size = 3
+  ) +
+  geom_path(
+    data = tibble(x = rep(salvador, 2),y = c(0, height_salvador)),
+    mapping = aes(x = x, y = y), linetype = 'dotted', alpha = .8,
+  ) +
+  geom_label(
+    x = salvador, y = height_salvador, color = 'gray15', fill = 'gray97',
+    label = str_c('Salvador:\n', round(salvador, 1)) %>% 
+      str_replace('\\.', ','),
+    family = 'serif', size = 3
   ) +
   theme(panel.grid = element_blank(),
         legend.title = element_blank(),
         legend.position = c(.17, .90)) +
   scale_fill_manual(values = c('#fed976', '#ef3b2c')) +
-  scale_x_continuous(breaks = seq(20, 70, by = 10)) +
-  coord_cartesian(xlim = c(20, 70)) +
+  scale_x_continuous(breaks = seq(0, 100, by = 10)) +
+  # coord_cartesian(xlim = c(20, 70)) +
   labs(
     x = 'Média dos indicadores normalizados (0 a 100)',
     y = 'Número de municípios',
     title = 'Qualidade dos serviços de fornecimento de água',
     subtitle = 'Distribuição das notas dos municípios baianos'
-  ) ; hist_ranking_agua
+  )
+
+hist_ranking_agua
 
 # Saving
 ggsave(plot = hist_ranking_agua, width = 6, height = 5,
-       filename = 'plots/histogram-ranking-agua.png')
+       filename = 'plots/ranking/histogram-ranking-agua.png')
 
-# Average by provider type
-snis_ranking %>% 
+# Average by provider type ----------------------------------------------------
+avg_by_provider_type <- snis_ranking %>% 
   group_by(tipo_prestador) %>% 
-  summarise(media_ponderada = weighted.mean(score, w = populacao),
-            media_simples = mean(score)) %>% 
-  ungroup()
+  summarise(avg_pop = mean(populacao),
+            median_pop = median(populacao),
+            media_simples = mean(score),
+            media_ponderada = weighted.mean(score, w = populacao),
+            mediana = median(score)) %>% 
+  ungroup() ; avg_by_provider_type
+
+# Saving
+write_excel_csv2(avg_by_provider_type,
+                 'data/ranking/score-agua-avg-provider-type.csv')
+
